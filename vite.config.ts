88a9a84ccf0d6bf966005d8bb9a9b959858021b3
Ken Snyder <ken@ken.net>
2022-12-09 10:55:02 -0800

    chore: removed "types" from ESM export block

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,7 @@ import meta from "./src";
 
 // used for testing, library code uses TSUP to build exports
 export default defineConfig({
-  test: {
-    dir: "test",
-    environment: "happy-dom",
-  },
+
   plugins: [
     Markdown({ builders: [meta()] }),
     Vue({
